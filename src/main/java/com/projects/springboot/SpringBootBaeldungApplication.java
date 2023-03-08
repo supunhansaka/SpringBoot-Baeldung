6294c2eb038c60863297779191500e36967c16d8
Supun Hansaka <100472049+IT20653118@users.noreply.github.com>
2023-03-09 00:36:37 +0530

    springboot-baeldung project initialize

---
+++ src/main/java/com/projects/springboot/SpringBootBaeldungApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBaeldungApplication.class, args);
+	}
+
+}
